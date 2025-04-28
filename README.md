87dbceb3dd03bd7665043afad5035e7ad7028902
Yong Eun Jang <jys613k@gmail.com>
2025-04-28 10:53:57 +0900

    add1 commit

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# eCPPpred
+# CPPpred-En
+
+CPPpred-En is an ensemble-based prediction model for identifying cell-penetrating peptides (CPPs), which are crucial for drug delivery and peptide-based therapeutics. Unlike previous methods that rely on either conventional features or a limited number of protein language models (PLMs), CPPpred-En combines 69 conventional features with 8 PLM-derived features across 8 machine learning classifiers. The final model, built using an ensemble strategy, demonstrates strong generalizability and achieves state-of-the-art performance with an accuracy of 97.27% (MCC 0.964) on balanced and 96.10% (MCC 0.707) on imbalanced datasets.
+
+
+## Model workflow
+(./image/Figure.png)
+
+## Installation Guide
+
+This code requires Python 3. For simplicity, I recommend using Anaconda. If conda is not installed on your system, you can download it [here](https://docs.anaconda.com/miniconda/).
+
+1. (Optional) Create a conda environment for this workshop and activate it:
+
+```bash
+conda create -n CPPpred-En python=3.9.20
+source activate CPPpred-En
+```
+
+2. Install the environment:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Dataset
+All datasets are located in the [data folder](./data).
+CPP924: Download Standalone Version of [CPP924](https://github.com/Excelsior511/StackCPPred)
+MLCPP2.0: Download Standalone Version of [MLCPP2.0](https://balalab-skku.org/mlcpp2/download/)
+
+
+## Usage
+
+How to run the CPP924(balance) dataset using the pre-trained weights:
+
+```bash
+python balance_data_test.py
+```
+
+How to run the MLCPP2.0(imbalance) set using the pre-trained weights:
+
+```bash
+python imbalance_data_test.py
+```
+
+## Author
+
+Yong Eun Jang, Minjun Kwon, Seok Gi Kim , Nimisha Pradeep George, Ji Su Hwang, Shaherin Basith , Gwang Lee
+## Reference
+
+To be added once the paper is published.
--- balance_data_test.py
+++ balance_data_test.py
@@ -8,9 +8,8 @@ import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 test_path = "./CPP_feature/CPP_test/"
-save_path = "./selected_weight/CPP/"  # 저장된 모델 가중치 파일 경로
+save_path = "./selected_weight/CPP/"  
 
-# 모델 로드
 models = {
     "model_cat1": joblib.load(os.path.join(save_path, "model_cat1_TPC type 1_best_model.pkl")),
     "model_ERT2": joblib.load(os.path.join(save_path, "model_ERT2_prot_t5_xl_bfd_best_model.pkl")),
@@ -20,7 +19,6 @@ models = {
     "model_gb2": joblib.load(os.path.join(save_path, "model_gb2_esm1v_best_model.pkl"))
 }
 
-# 테스트 파일 목록과 모델 매칭
 test_files = {
     "model_cat1": "TPC type 1.csv",
     "model_ERT2": "prot_t5_xl_bfd.csv",
@@ -30,7 +28,6 @@ test_files = {
     "model_gb2": "esm1v.csv"
 }
 
-# 테스트 데이터 로드 및 레이블 설정
 test_data = {}
 labels = None
 
@@ -43,32 +40,27 @@ for model_name, test_file in test_files.items():
     
     test_data[model_name] = data.drop(['name', 'target'], axis=1)
 
-# 모델 확률 예측 저장
 model_probabilities = []
 
 for model_name in models:
     model = models[model_name]
     X_test = test_data[model_name]
     
-    # 확률 예측 수행
     if hasattr(model, "predict_proba"):
         probs = model.predict_proba(X_test)[:, 1]
         model_probabilities.append(probs)
     else:
         raise ValueError(f"Model {model_name} does not support predict_proba.")
 
-# 소프트 보팅 기반 앙상블 확률 계산
 ensemble_proba = np.mean(model_probabilities, axis=0)
 final_predictions = (ensemble_proba >= 0.5).astype(int)
 
-# 앙상블 모델 성능 평가
 final_accuracy = accuracy_score(labels, final_predictions)
 final_mcc = matthews_corrcoef(labels, final_predictions)
 tn, fp, fn, tp = confusion_matrix(labels, final_predictions).ravel()
 final_sensitivity = tp / (tp + fn) if (tp + fn) > 0 else 0
 final_specificity = tn / (tn + fp) if (tn + fp) > 0 else 0
 
-# 앙상블 모델 성능 출력
 print("\nEnsemble Model Performance Metrics:")
 print(f"  Accuracy: {final_accuracy:.4f}")
 print(f"  Sensitivity: {final_sensitivity:.4f}")
